dfca0a3bb2c7631f8d633badd943eb4bdc9ef27e
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2024-10-29 21:34:58 +0200

    Добавьте файлы проекта.

---
+++ Компьютерный практикум 4.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Компьютерный_практикум_4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+                int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+                int b = int.Parse(Console.ReadLine());
+            Console.Write("c=");
+                int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"a+b+c =  {a + b + c}");
+            Console.ReadKey();
+
+        }
+    }
+}
